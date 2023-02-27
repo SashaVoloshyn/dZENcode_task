20926dc01f60ce2163c6dd6c127cd31e98c8e37d
SashaVoloshyn <sasha.voloshyn359@gmail.com>
2023-02-27 13:47:55 +0200

    add client

---
+++ client/src/components/TagsBlock.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import TagIcon from '@mui/icons-material/Tag'
+import ListItemText from '@mui/material/ListItemText'
+import Skeleton from '@mui/material/Skeleton'
+
+import { SideBlock } from './SideBlock'
+
+export const TagsBlock = ({ items, isLoading = true }) => {
+    return <></>
+}
